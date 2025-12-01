bd07fbd49a301450c04efe34a72fec8a254d73db
weyilham <dilhammaulanaa@gmail.com>
2025-12-01 21:16:12 +0700

    pindah ke file api

--- api/index.js
+++ api/index.js
@@ -1,3 +1,27 @@
-import app from "../src/server.js";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "../src/routes/authRoutes.js";
+import inventoryRoutes from "../src/routes/inventoryRoutes.js";
+import productRoutes from "../src/routes/productRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use("/auth", authRoutes);
+app.use("/inventory", inventoryRoutes);
+app.use("/products", productRoutes);
+
+// fallback (optional)
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
 export default app;
