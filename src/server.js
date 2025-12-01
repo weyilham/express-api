022f6ff14e5e3d673e80beed531a627f1a8a6159
weyilham <dilhammaulanaa@gmail.com>
2025-12-01 20:52:36 +0700

    persiapan deploy vercel

--- src/server.js
+++ src/server.js
@@ -46,6 +46,10 @@ app.use((req, res, next) => {
 });
 
 // start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
 });
